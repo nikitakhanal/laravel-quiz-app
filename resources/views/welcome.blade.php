b92b20305f678db7cd374ae636c46d6e8401d10b
nikitakhanal <nikitakhanal30@gmail.com>
2023-05-13 18:49:55 +0545

    refactor: welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,47 +8,65 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  
+    <link rel="stylesheet" href="{{asset('css/welcome.css')}}">
   </head>
   <body>
-      
-    <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#user">
-    Play
-  </button>
-  
-  <!-- Modal -->
-  <div class="modal fade" id="user" tabindex="-1" role="dialog" aria-labelledby="userLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="userLabel">Play</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
+
+    <div class="container">
+
+      <div class="headline">
+        QUIZ, TRIVIA
+      </div>
+
+      <p class="rules">
+        <ul>
+          <p>How to play?</p>
+          <li>You'll be asked 10 questions</li>
+          <li>Each question comes with 4 options</li>
+          <li>You're given 45 seconds to answer each question</li>
+          <li>Click the option you think is correct</li>
+        </ul>
+      </p>
+
+       <!-- Button trigger modal -->
+      <button type="button" class="btn btn-primary play-CTA" data-toggle="modal" data-target="#user">
+        Click Here To Play
+      </button>
+
+      <!-- Modal -->
+      <div class="modal fade" id="user" tabindex="-1" role="dialog" aria-labelledby="userLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="userLabel">Play</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
 
 
-            {{-- form starts --}}
-          <form action="{{URL('player')}}" class="userForm" method="POST">
-            @csrf
-            <div class="form-group row">
-                <label class="col-sm-2 col-form-label" for="username">Username: </label>
-                <div class="col-sm-10">
-                    <input class="form-control" type="text" name="username" id="username">
+                {{-- form starts --}}
+              <form action="{{URL('player')}}" class="userForm" method="POST">
+                @csrf
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label" for="username">Username: </label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" name="username" id="username">
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="modal-footer">
-            <button type="submit" class="btn btn-primary">Start</button>
-            <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
-        </form>
+            <div class="modal-footer">
+                <button type="submit" class="btn btn-primary">Start</button>
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+            </form>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
 
-
+    </div> <!-- container ends -->
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
